c433d137e19f7cebe61429223def99834f12a776
Alysson Bormann <alysson.bormann@gmail.com>
2022-03-12 14:00:22 -0300

    criando conta e autenticando

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,11 +1,46 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Alert } from "react-native";
+import auth from "@react-native-firebase/auth";
 
-import { Container, Account, Title, Subtitle } from './styles';
-import { ButtonText } from '../../components/ButtonText';
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
+import { Container, Account, Title, Subtitle } from "./styles";
+import { ButtonText } from "../../components/ButtonText";
+import { Button } from "../../components/Button";
+import { Input } from "../../components/Input";
 
 export function SignIn() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleCreateUserAccount() {
+    auth()
+      .createUserWithEmailAndPassword(email, password)
+      .then(() => Alert.alert("Usuário criado com sucesso!"))
+      .catch((error) => {
+        console.log(error.code);
+        if (error.code === "auth/email-already-in-use") {
+          Alert.alert("E-mail já cadastrado.");
+        }
+      });
+  }
+
+  function handleSignInWithEmailAndPassword() {
+    auth()
+      .signInWithEmailAndPassword(email, password)
+      .then((user) => {
+        console.log(user);
+      })
+      .catch((error) => {
+        console.log(error.code);
+
+        if (
+          error.code === "auth/user-not-found" ||
+          error.code === "auth/wrong-passowrd"
+        ) {
+          Alert.alert("Usuário não encontrado. E-mail e/ou senha inválida!");
+        }
+      });
+  }
+
   return (
     <Container>
       <Title>MyShopping</Title>
@@ -14,18 +49,19 @@ export function SignIn() {
       <Input
         placeholder="e-mail"
         keyboardType="email-address"
+        onChangeText={setEmail}
       />
 
-      <Input
-        placeholder="senha"
-        secureTextEntry
-      />
+      <Input placeholder="senha" secureTextEntry onChangeText={setPassword} />
 
-      <Button title="Entrar" onPress={() => { }} />
+      <Button title="Entrar" onPress={handleSignInWithEmailAndPassword} />
 
       <Account>
-        <ButtonText title="Recuperar senha" onPress={() => { }} />
-        <ButtonText title="Criar minha conta" onPress={() => { }} />
+        <ButtonText title="Recuperar senha" onPress={() => {}} />
+        <ButtonText
+          title="Criar minha conta"
+          onPress={handleCreateUserAccount}
+        />
       </Account>
     </Container>
   );
